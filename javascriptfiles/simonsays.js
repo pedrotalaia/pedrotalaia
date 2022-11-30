bf28e796084b31385134eba06b6db3643f3c572f
pedrotalaia <josepedrotalaia@gmail.com>
2022-11-30 22:51:28 +0100

    simonsays changes

--- javascriptfiles/simonsays.js
+++ javascriptfiles/simonsays.js
@@ -1,4 +1,23 @@
+var colorArray = [];
+document.addEventListener('click', colorPicked)
 
 function play (){
-    alert('Game Started!')
+alert('Game Started!')
+var randomNumberGenerator = Math.floor(Math.random()*4);
+var colors = ['green', 'blue', 'red', 'yellow']
+colorArray.push(colors[randomNumberGenerator]);
+console.log(colorArray);
+
+// ARRAY ITERATOR FOR THE FUTURE
+// colorArray.forEach(el => {
+//     el === 'green' ? console.log(el) : console.log('not green');
+//     el === 'yellow' ? console.log(el) : console.log ('not yellow');
+// });
+
 };
+
+function colorPicked () {
+    var colorClicked = document.querySelector('btn');
+    return colorClicked;
+}
+
