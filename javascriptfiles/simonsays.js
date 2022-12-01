af227bfe0eeb2fa3dbd087bc98ac0e9bb74c7b74
pedrotalaia <josepedrotalaia@gmail.com>
2022-12-01 23:00:00 +0100

    simonsays new changes

--- javascriptfiles/simonsays.js
+++ javascriptfiles/simonsays.js
@@ -1,23 +1,42 @@
 var colorArray = [];
-document.addEventListener('click', colorPicked)
 
-function play (){
-alert('Game Started!')
-var randomNumberGenerator = Math.floor(Math.random()*4);
-var colors = ['green', 'blue', 'red', 'yellow']
-colorArray.push(colors[randomNumberGenerator]);
-console.log(colorArray);
+document.addEventListener('click', (event) => {
+    return colorPicked(event.target.id);
+});
+var randomNumberGenerator = Math.floor(Math.random() * 4);
 
-// ARRAY ITERATOR FOR THE FUTURE
-// colorArray.forEach(el => {
-//     el === 'green' ? console.log(el) : console.log('not green');
-//     el === 'yellow' ? console.log(el) : console.log ('not yellow');
-// });
+function play() {
+    alert('Game Started!')
+    var colors = ['green', 'blue', 'red', 'yellow']
+    colorArray.push(colors[randomNumberGenerator]);
+    console.log(colorArray);
+
+    // ARRAY ITERATOR FOR THE FUTURE
+    // colorArray.forEach(el => {
+    //     el === 'green' ? console.log(el) : console.log('not green');
+    //     el === 'yellow' ? console.log(el) : console.log ('not yellow');
+    // });
 
 };
 
-function colorPicked () {
-    var colorClicked = document.querySelector('btn');
-    return colorClicked;
+function colorPicked(event) {
+    var buttonClicked = document.querySelectorAll('.btn');
+    
+    if (event === 'green_btn') {
+        try {
+            setTimeout(function changeStatus() {
+                buttonClicked[0].classList.add('changeColor');
+            }, 100)
+            setTimeout(function returnStatus() {
+                buttonClicked[0].classList.remove('changeColor');
+            }, 100)
+        } catch (msg) {
+            console.log(msg)
+        };
+    }
+    else {
+    console.log('other button ' + event)
+    }
+
 }
 
